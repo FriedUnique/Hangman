493760e4caa4d313a0c8dd789ecd1839cab51cf1
FriedUnique <stefan.ochsner@live.com>
2022-05-06 14:44:03 +0200

    Update: - New Wordgen and display - Renamed Files

--- Hangman.py
+++ Hangman.py
@@ -1,18 +1,51 @@
-import os
 import pygame
-import math
-import sys
-import random
-import time
+import os, math
 
+from random import choice
+from time import thread_time
 from typing import List
 from dataclasses import dataclass
 
-# setup display
 pygame.init()
-WIDTH, HEIGHT = 1100, 700 # minimum: 700, 450, perfect: 1200 750
-win = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# region load
+# loading the words from the woerter.txt file.
+# have to load above the screen init so you can edit the screen
+allWords: List[str] = []
+largestWord = 0
+with open("woerter.txt", "r") as f:
+    allWords = f.read().upper().split(",")
+for i in range(len(allWords)):
+    allWords[i] = allWords[i].strip()
+
+print(allWords)
+
+# finding the largest word
+for w in allWords:
+    x = pygame.font.SysFont('comicsans', 60).size(w)
+    largestWord = x[0] if largestWord < x[0] else largestWord
+# endregion
+
+
+#* pygame display init. adapting the screen to the longest possible word
+screenX, screenY = 800+max(largestWord-500, 0)+100, 500
+screen = pygame.display.set_mode((screenX, screenY))
 pygame.display.set_caption("Hangman")
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# fonts
+BUTTON_FONT = pygame.font.SysFont('comicsans', 40)
+WORD_FONT = pygame.font.SysFont('comicsans', 60)
+TITLE_FONT = pygame.font.SysFont('comicsans', 70)
+
+# game
+hangman_status = 0
+word = ""
+usedLetters = [] # if letter in usedLetters, it means it was pressed
+usedWords = [] # at the start of a round, a new word gets selected and marked as used.
 
 @dataclass
 class Button:
@@ -21,113 +54,119 @@ class Button:
     character: str
     visible: bool
 
-# colors
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-
-# fonts
-LETTER_FONT = pygame.font.SysFont('comicsans', 40)
-WORD_FONT = pygame.font.SysFont('comicsans', 60)
-TITLE_FONT = pygame.font.SysFont('comicsans', 70)
-
 # button variables
 buttons: List[Button] = []
 RADIUS = 20
 SPACE = 15 # between each button
-startx = round((WIDTH - (RADIUS * 2 + SPACE) * 13) / 2)
-starty = HEIGHT - 170
+startX = round((screenX - (RADIUS * 2 + SPACE) * 13) / 2)
+startY = screenY - 170
 
-# game
-hangman_status = 0
-word = ""
-guessed = []
+# load images.
+# orignal image size: 338 | 392 (rel: ~1.2)
+hangmanImages: List[pygame.Surface] = []
+x = startY-150
+for i in range(10):
+    image = pygame.image.load(os.path.join("images", "hangman (" + str(i+1) + ").png"))
+    image = pygame.transform.scale(image, (x, int(x*1.2))) # resize images
+    hangmanImages.append(image)
+
 
 def setupButtons():
     global buttons
     buttons = []
+    perRow = 13 # buttons per row
 
     for i in range(26):
-        # 13 pro zeile
-        x = startx + SPACE * 2 + ((RADIUS * 2 + SPACE) * (i % 13))
-        y = starty + ((i // 13) * (SPACE + RADIUS * 2))
+        x = startX + SPACE * 2 + ((RADIUS * 2 + SPACE) * (i % perRow))
+        y = startY + ((i // perRow) * (SPACE + RADIUS * 2))
         buttons.append(Button(x, y, chr(65 + i), True))
 
-# load images.
-images = []
-for i in range(10):
-    image = pygame.image.load(os.path.join("src", "backdrop" + str(i) + ".png"))
-    images.append(image)
+def newWord() -> str:
+    """Prevent that words wont show up multiple times after each other."""
+    global allWords, usedWords
+    res = [i for i in allWords if i not in usedWords]
 
-# game variables
-words = []
-with open("woerter.txt", "r") as f:
-    words = f.read().upper().replace(" ", "").split(",")
+    if len(res) == 0:
+        usedWords = []
+        usedWords.append(choice(allWords))
+        return usedWords[0]
 
-wordsString = " ".join(words)
+    usedWords.append(choice(res)) 
+    return usedWords[len(usedWords)-1]
 
 def reset():
-    global hangman_status, guessed, word
-    guessed = []
-    word = random.choice(words)
+    global hangman_status, usedLetters, word
+    usedLetters = []
+    word = newWord()
     hangman_status = 0
     setupButtons()
 
-
 def draw():
-    win.fill(WHITE)
-    # draw title
-    text = TITLE_FONT.render("Hangman", True, BLACK)
-    win.blit(text, (int(WIDTH/2) - int(text.get_width()/2), 20)) # titel
+    global screenX, screenY
+    screen.fill(WHITE)
 
-    # draw word
-    display_word = ""
+    # compile guessWord
+    guessWord = ""
     for letter in word:
-        # if letter in guessed, it means it was pressed
-        if letter in guessed:
-            display_word += letter + " "
+        if letter in usedLetters:
+            guessWord += letter + " "
         else:
-            display_word += "_ "
-    text = WORD_FONT.render(display_word, True, BLACK)
-    win.blit(text, (int(WIDTH/2) - int(text.get_width()/2), int(HEIGHT/2) - int(text.get_height()/2)))# lösung
+            # add space
+            if ord(letter) not in range(65, 91):
+                guessWord += letter + " "
+                continue
+
+            guessWord += "_ "
+
 
     # draw buttons
     for button in buttons:
         if button.visible:
-            r = pygame.Rect(button.x-RADIUS, button.y-RADIUS, RADIUS*2, RADIUS*2)
-            pygame.draw.rect(win, BLACK, r, 5)
-            text = LETTER_FONT.render(button.character, True, BLACK)
-            win.blit(text, (int(button.x) - int(text.get_width()/2), int(button.y) - int(text.get_height()/2)))
+            rect = pygame.Rect(button.x-RADIUS, button.y-RADIUS, RADIUS*2, RADIUS*2)
+            pygame.draw.rect(screen, BLACK, rect, 5)
+            t = BUTTON_FONT.render(button.character, True, BLACK)
+            screen.blit(t, (int(button.x) - int(t.get_width()/2), int(button.y) - int(t.get_height()/2)))
 
-    win.blit(images[hangman_status], (150, 100))
+    # hangman, rendered first, so if the text overlaps it will be more or less visible
+    screen.blit(hangmanImages[hangman_status], (25, 50))
+
+    # Title
+    t = TITLE_FONT.render("Hangman", True, BLACK)
+    screen.blit(t, (int(screenX/2) - int(t.get_width()/2), 20))
+
+    # guessWord
+    t = WORD_FONT.render(guessWord, True, BLACK)
+    screen.blit(t, (int(screenX/2) - int(t.get_width()/2), int(screenY/2) - int(t.get_height()/2)))
+
     pygame.display.update()
 
-
 def display_message(message, waitTime=3):
-    """WaitTime in sekunden angeben"""
+    """WaitTime in seconds"""
     global run
-    tStart = time.thread_time()
-    tEnd = tStart + waitTime
+    tStart = thread_time()
+    tEnd = tStart + waitTime + 1
 
-    while time.thread_time() < tEnd:
+    pygame.time.wait(1000) #wait one second (ns) so the user can see the Hangman when it is complete.
+
+    while thread_time() < tEnd:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                print("break?")
+                print("exit in wait screen!")
                 return
         
-        win.fill(WHITE)
+        screen.fill(WHITE)
         text = WORD_FONT.render(message, True, BLACK)
-        win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+        screen.blit(text, (int(screenX/2) - int(text.get_width()/2), int(screenY/2) - int(text.get_height()/2)))
 
         pygame.display.update()
 
 
-reset()
 run = True
-
 def main():
     global hangman_status, run
     
+    reset()
     FPS = 60
     clock = pygame.time.Clock()
 
@@ -138,32 +177,37 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                m_x, m_y = pygame.mouse.get_pos()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouseX, mouseY = pygame.mouse.get_pos()
                 for button in buttons:
                     if button.visible:
-                        #calculate the overlap point of mouse pos and button
-                        distance = math.sqrt((button.x - m_x)**2 + (button.y - m_y)**2)
-                        if distance < RADIUS:
+                        # calculate the overlap of mouse pos and button
+                        distance = math.sqrt((button.x - mouseX)**2 + (button.y - mouseY)**2)
+                        if distance <= RADIUS:
                             button.visible = False
-                            guessed.append(button.character)
+                            usedLetters.append(button.character)
                             if button.character not in word:
                                 hangman_status += 1
-
         draw()
 
         won = True
         for letter in word:
-            if letter not in guessed:
+            # only capital letters can be guessed and therefore the win condition is that only these have to be guessed.
+            if ord(letter) not in range(65, 91): continue
+
+            if letter not in usedLetters:
                 won = False
                 break
         
+        # game ending conditions
         if won:
             display_message('Gewonnen')
             reset()
-        if hangman_status == 9:
+            continue
+        elif hangman_status == len(hangmanImages)-1:
             display_message('Verloren')
             reset()
+            continue
     
 
 main()
